2caa24086d4f5acdf7249782fa3a9699f1b1fe6a
Matthieu <matthieu.varagnat@gmail.com>
2025-09-05 09:26:29 +0200

    add evo resources

---
+++ app/avo/resources/game_event.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Avo
+  module Resources
+    class GameEvent < Avo::BaseResource
+      self.model_class = ::Game::Event
+      self.title = :id
+      self.search = {
+        query: -> { query.ransack(id_eq: q, m: 'or').result(distinct: false) }
+      }
+
+      def fields
+        field :id, as: :id
+        field :game_system, as: :belongs_to, resource: Avo::Resources::GameSystem
+        field :played_at, as: :date_time, required: true
+        field :elo_applied, as: :boolean
+        field :metadata, as: :code, language: 'json'
+
+        field :game_participations, as: :has_many, resource: Avo::Resources::GameParticipation
+        field :players, as: :has_many, through: :game_participations, resource: Avo::Resources::User
+      end
+    end
+  end
+end
