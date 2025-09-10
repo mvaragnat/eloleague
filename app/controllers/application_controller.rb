8ace046584f417f2b4717cdc83dafcc3e77c3ff9
Matthieu Varagnat <matthieu.varagnat@gmail.com>
2025-09-10 11:01:44 +0200

    fix Fr locale (#87)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!, unless: :devise_controller?
   before_action :set_locale
+  before_action :redirect_to_cookie_locale_if_missing
   before_action :set_current_user
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -30,12 +31,36 @@ class ApplicationController < ActionController::Base
   end
 
   def set_locale
-    I18n.locale = extract_locale || I18n.default_locale
+    chosen = extract_locale
+    chosen ||= cookies[:locale]
+    chosen = chosen.to_s if chosen
+    available = I18n.available_locales.map(&:to_s)
+    chosen = I18n.default_locale unless available.include?(chosen)
+
+    I18n.locale = chosen
+
+    # Persist when explicitly selected via URL
+    return unless params[:locale].present? && params[:locale].to_s != cookies[:locale]
+
+    cookies[:locale] = { value: params[:locale].to_s, expires: 1.year.from_now }
   end
 
   def extract_locale
     parsed_locale = params[:locale]
     I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
+
+  def redirect_to_cookie_locale_if_missing
+    return if params[:locale].present?
+    return unless request.get? && request.format.html?
+
+    cookie_locale = cookies[:locale]
+    return if cookie_locale.blank?
+
+    available = I18n.available_locales.map(&:to_s)
+    return unless available.include?(cookie_locale)
+
+    redirect_to url_for(locale: cookie_locale)
   end
 
   def default_url_options
