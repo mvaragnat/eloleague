6a56fc7697dcc42f987e2ef5d3c4e426f5c20b54
Matthieu <matthieu.varagnat@gmail.com>
2025-11-04 15:14:33 +0100

    fix scroll lists

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,11 +1,32 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  connect() {
+    this.originalBodyOverflow = document.body.style.overflow
+    this.originalBodyPaddingRight = document.body.style.paddingRight
+
+    const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth
+    if (scrollbarWidth > 0) {
+      document.body.style.paddingRight = `${scrollbarWidth}px`
+    }
+    document.body.style.overflow = 'hidden'
+  }
+
+  disconnect() {
+    this.unlockScroll()
+  }
+
   close(event) {
     event?.preventDefault()
     const frame = document.getElementById('modal')
     if (frame) {
       frame.innerHTML = ''
     }
+    this.unlockScroll()
   }
-} 
+
+  unlockScroll() {
+    document.body.style.overflow = this.originalBodyOverflow || ''
+    document.body.style.paddingRight = this.originalBodyPaddingRight || ''
+  }
+}
