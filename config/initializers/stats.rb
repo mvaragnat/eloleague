b13e0be2c843b9d9b89746d4f54953ba5ace6a7e
Matthieu <matthieu.varagnat@gmail.com>
2025-11-10 12:45:11 +0100

    modify min stats

--- config/initializers/stats.rb
+++ config/initializers/stats.rb
@@ -4,7 +4,7 @@
 Rails.application.config.x.stats = ActiveSupport::OrderedOptions.new unless Rails.application.config.x.respond_to?(:stats)
 Rails.application.config.x.stats.min_players = ENV.fetch('STATS_MIN_PLAYERS', '4').to_i
 Rails.application.config.x.stats.min_games = ENV.fetch('STATS_MIN_GAMES', '10').to_i
-Rails.application.config.x.stats.min_matchup_players = [Rails.application.config.x.stats.min_players - 4, 2].max
-Rails.application.config.x.stats.min_matchup_games = [Rails.application.config.x.stats.min_games - 4, 5].max
+Rails.application.config.x.stats.min_matchup_players = [Rails.application.config.x.stats.min_players - ENV.fetch('STATS_MATCHUP_DISCOUNT', '4').to_i, 2].max
+Rails.application.config.x.stats.min_matchup_games = [Rails.application.config.x.stats.min_games - ENV.fetch('STATS_MATCHUP_DISCOUNT', '4').to_i, 2].max
 
 
